bb29c95455b3b7d951ea85651a7e7d855defd83c
Lura Landon <lura.landon@gmail.com>
2019-06-29 17:10:33 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,14 @@
+counter = 0 
+loop do
+  counter = counter + 1
+  puts "iteration #{counter} of the loop"
+  if counter >= 10 
+    break
+  end
+end
+
+counter2 = 10
+while counter2 >= 1 
+puts "iteration #{counter2} of the loop"
+counter2 = counter2 - 1 
+end
